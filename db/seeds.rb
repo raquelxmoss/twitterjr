40b0b6223cb59d6344b729da92f00ef352c0e446
Geordi Dearns <apprentice@StMac3.local>
2015-03-26 16:50:27 +1300

    Added bio to table, edited seed file to match changes  and tested seed - working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,15 @@ Retweet.delete_all
 
 # Seed Users
 10.times do
-  User.create( email: Faker::Internet.email )
+  options = {
+    handle: Faker::Lorem.word,
+    email: Faker::Internet.email,
+    full_name: Faker::Name.name,
+    bio: Faker::Lorem.sentence,
+    password: Faker::Internet.password,
+    gravatar: Faker::Internet.url
+  }
+  User.create(options)
 end
 
 #seed Tweets

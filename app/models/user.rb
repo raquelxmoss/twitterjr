7f6150560221ce6e68244d42693ab53f74f0f140
Geordi Dearns <apprentice@StMac3.local>
2015-03-26 15:05:09 +1300

    Added sign up and login, authentication/encryption model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
+require 'scrypt'
+
 class User < ActiveRecord::Base
   # Remember to create a migration!
   has_many :tweets
+
+  before_save do
+    encrypt_password
+  end
+
+  def encrypt_password
+    self.password = SCrypt::Password.create(password)
+  end
+
+  def self.authenticate(handle, password)
+    if user = User.find_by(handle: handle)
+      return user if SCrypt::Password.new(user.password) == password
+    end
+    return nil
+  end
+
 end

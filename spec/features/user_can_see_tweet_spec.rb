5cefb4c8667032f44b1a476cb67d7292eb7f5728
Geordi Dearns <apprentice@StMac3.local>
2015-03-27 10:19:43 +1300

    Added capybara test for sign up process

--- spec/features/user_can_see_tweet_spec.rb
+++ spec/features/user_can_see_tweet_spec.rb
@@ -1,6 +1,23 @@
 require_relative '../spec_helper'
 require 'faker'
 
+
+describe "the signin process", :type => :feature do
+  before :each do
+    User.create(:handle => 'geordi', :password => 'password')
+  end
+
+  it "signs me in" do
+    visit '/'
+    within ('#login-form') do
+      fill_in 'handle', :with => 'geordi'
+      fill_in 'password', :with => 'password'
+    end
+    click_button 'Login'
+    expect(page).to have_content ''
+  end
+end
+
 describe 'user can see tweet', feature: true do
 
   before do

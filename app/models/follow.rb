4efe9a4413eaff3aebe5e9d82532082f7e753935
Alexandre Barret <barretalexandre@hotmail.com>
2015-03-26 15:23:06 +1300

    Create tests for the follower followee relationships

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,6 +2,6 @@ class Follow < ActiveRecord::Base
   belongs_to :follower, class_name: 'User'
   belongs_to :followee, class_name: 'User'
 
-  validates_uniqueness_of :follower_id, scope: :followee_id #make sure ever follower id is
+  validates_uniqueness_of :follower_id, scope: :followee_id
   validates_uniqueness_of :followee_id, scope: :follower_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  has_many :follows
+  has_many :followee_follows, class_name: 'Follow', foreign_key: 'followee_id'
+  has_many :followers, class_name: 'User', through: :followee_follows
 
-
-  has_many :followers, class_name: 'User', through: :follows
-  # has_many :followees, class_name: 'User', through: :follows
+  has_many :follower_follows, class_name: 'Follow', foreign_key: 'follower_id'
+  has_many :followees, class_name: 'User', through: :follower_follows
 
   has_many :tweets
 end

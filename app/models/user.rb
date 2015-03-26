dbdef8d553605af9380b3885c6454e3cccb18c84
Alexandre Barret <barretalexandre@hotmail.com>
2015-03-26 14:50:51 +1300

    Create has many through users as followers and followees

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  has_many :followings, foreing_key: 'follower_id'
+  has_many :followers, class_name: 'User', through: :followings,  foreing_key: 'follower_id'
+  has_many :followees, class_name: 'User', through: :followings,  foreing_key: 'followee_id'
   has_many :tweets
 end
--- db/migrate/20150326143302_create_followings.rb
+++ db/migrate/20150326143302_create_followings.rb
@@ -1,4 +1,12 @@
 class CreateFollowings < ActiveRecord::Migration
   def change
+
+    create_table :followings do |t|
+      t.integer :follower_id
+      t.integer :followee_id
+
+      t.timestamps null: false
+    end
+
   end
 end
